c6212b08b3396c80bfd1583775c4801a6cf55b10
Bodigrim <andrew.lelechenko@gmail.com>
2022-04-22 23:35:33 +0100

    Benchmark against text-builder

--- bench/Main.hs
+++ bench/Main.hs
@@ -13,6 +13,10 @@ import Data.Text.Lazy.Builder.Int (decimal, hexadecimal)
 import Data.Text.Lazy.Builder.RealFloat (realFloat)
 import Test.Tasty.Bench
 
+#ifdef MIN_VERSION_text_builder
+import qualified Text.Builder
+#endif
+
 txt ∷ T.Text
 txt = T.pack "Haskell + Linear Types = ♡"
 
@@ -25,12 +29,21 @@ int = 123456789123456789
 dbl :: Double
 dbl = - pi * 1e300
 
-benchBuilder ∷ Int → T.Text
-benchBuilder = toStrict . toLazyText . go mempty
+benchLazyBuilder ∷ Int → T.Text
+benchLazyBuilder = toStrict . toLazyText . go mempty
   where
     txtB = fromText txt
     go !acc 0 = acc
     go !acc n = go (txtB <> (acc <> txtB)) (n - 1)
+
+#ifdef MIN_VERSION_text_builder
+benchStrictBuilder ∷ Int → T.Text
+benchStrictBuilder = Text.Builder.run . go mempty
+  where
+    txtB = Text.Builder.text txt
+    go !acc 0 = acc
+    go !acc n = go (txtB <> (acc <> txtB)) (n - 1)
+#endif
 
 benchLinearBuilder ∷ Int → T.Text
 benchLinearBuilder m = runBuffer (\b → go b m)
@@ -44,7 +57,11 @@ main = defaultMain $ map mkGroup [1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6]
 
 mkGroup :: Int → Benchmark
 mkGroup n = bgroup (show n)
-  [ bench "Data.Text.Lazy.Builder" $ nf benchBuilder n
+  [ bench "Data.Text.Lazy.Builder" $ nf benchLazyBuilder n
+#ifdef MIN_VERSION_text_builder
+  , bcompare ("$NF == \"Data.Text.Lazy.Builder\" && $(NF-1) == \"" ++ show n ++ "\"")
+  $ bench "Text.Builder" $ nf benchStrictBuilder n
+#endif
   , bcompare ("$NF == \"Data.Text.Lazy.Builder\" && $(NF-1) == \"" ++ show n ++ "\"")
   $ bench "Data.Text.Builder.Linear" $ nf benchLinearBuilder n
   ]
