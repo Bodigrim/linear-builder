223f246f88b9677ba8e27118a179dd6c9c7ae72e
Bodigrim <andrew.lelechenko@gmail.com>
2022-08-11 01:08:51 +0100

    Switch to tasty-bench-0.3.2

--- bench/Main.hs
+++ bench/Main.hs
@@ -5,11 +5,8 @@
 
 module Main where
 
-import Data.Foldable (foldl')
 import Test.Tasty.Bench
-import Test.Tasty.Runners (TestTree(..))
 import Test.Tasty.Patterns.Printer
-import Test.Tasty.Patterns.Types as Pat
 
 import BenchChar
 import BenchDecimal
@@ -18,7 +15,7 @@ import BenchHexadecimal
 import BenchText
 
 main ∷ IO ()
-main = defaultMain $ map (mapLeafs addCompare) $
+main = defaultMain $ map (mapLeafBenchmarks addCompare) $
   [ benchText
   , benchChar
   , benchDecimal
@@ -31,22 +28,5 @@ textBenchName = "Data.Text.Lazy.Builder"
 
 addCompare :: ([String] -> Benchmark -> Benchmark)
 addCompare (name : path)
-  | name /= textBenchName = bcompare (printAwkExpr (locateLeaf (textBenchName : path)))
+  | name /= textBenchName = bcompare (printAwkExpr (locateBenchmark (textBenchName : path)))
 addCompare _ = id
-
-mapLeafs :: ([String] -> Benchmark -> Benchmark) -> Benchmark -> Benchmark
-mapLeafs processLeaf = go mempty
-  where
-    go :: [String] -> Benchmark -> Benchmark
-    go path = \case
-      SingleTest name t    -> processLeaf (name : path) (SingleTest name t)
-      TestGroup name tts   -> TestGroup name (map (go (name : path)) tts)
-      PlusTestOptions g tt -> PlusTestOptions g (go path tt)
-      WithResource res f   -> WithResource res (go path . f)
-      AskOptions f         -> AskOptions (go path . f)
-      After dep expr tt    -> After dep expr (go path tt)
-
-locateLeaf :: [String] -> Expr
-locateLeaf path
-  = foldl' And (IntLit 1)
-  $ zipWith (\i name -> Pat.EQ (Field (Sub NF (IntLit i))) (StringLit name)) [0..] path
