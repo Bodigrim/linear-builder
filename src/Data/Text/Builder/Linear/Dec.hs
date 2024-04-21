000ec42a532e908238983582a70f858e1610434a
Pierre Le Marre <dev@wismill.eu>
2024-04-21 08:57:08 +0000

    decimal: Add support for unbounded numbers

--- src/Data/Text/Builder/Linear/Dec.hs
+++ src/Data/Text/Builder/Linear/Dec.hs
@@ -10,8 +10,13 @@
 #endif
 
 module Data.Text.Builder.Linear.Dec (
+  -- * Bounded
   (|>$),
   ($<|),
+
+  -- * Unbounded
+  (|>$$),
+  ($$<|),
 ) where
 
 #include "MachDeps.h"
@@ -20,12 +25,19 @@ import Data.Bits (Bits (..), FiniteBits (..))
 import Data.Int (Int16, Int32, Int8)
 import Data.Text.Array qualified as A
 import Data.Word (Word16, Word32, Word8)
-import GHC.Exts (Addr#, Int (..), Ptr (..), dataToTag#, (>=#))
+import Foreign.C.String (CString)
+import GHC.Exts (Int (..), Ptr (..), Word (..), dataToTag#, isTrue#, (>#), (>=#))
+import GHC.Num.BigNat qualified as BN
+import GHC.Num.Integer qualified as I
 import GHC.Ptr (plusPtr)
 import GHC.ST (ST)
 import Numeric.QuoteQuot (assumeNonNegArg, astQuot, quoteAST, quoteQuot)
 
 import Data.Text.Builder.Linear.Core
+
+--------------------------------------------------------------------------------
+-- Bounded
+--------------------------------------------------------------------------------
 
 -- | Append the decimal representation of a /bounded/ integral number.
 (|>$) ∷ (Integral a, FiniteBits a) ⇒ Buffer ⊸ a → Buffer
@@ -34,7 +46,7 @@ infixl 6 |>$
 buffer |>$ n =
   appendBounded
     (maxDecLen n)
-    (\dst dstOff → unsafeAppendDec dst dstOff n)
+    (\dst dstOff → unsafeAppendBoundedDec dst dstOff n)
     buffer
 {-# INLINEABLE (|>$) #-}
 
@@ -45,8 +57,8 @@ infixr 6 $<|
 n $<| buffer =
   prependBounded
     (maxDecLen n)
-    (\dst dstOff → unsafePrependDec dst dstOff n)
-    (\dst dstOff → unsafeAppendDec dst dstOff n)
+    (\dst dstOff → unsafePrependBoundedDec dst dstOff n)
+    (\dst dstOff → unsafeAppendBoundedDec dst dstOff n)
     buffer
 {-# INLINEABLE ($<|) #-}
 
@@ -74,12 +86,12 @@ exactDecLen n
       | otherwise = I# (l# >=# 10_000#) + I# (l# >=# 1_000#) + I# (l# >=# 100#) + I# (l# >=# 10#)
 {-# INLINEABLE exactDecLen #-}
 
-unsafeAppendDec ∷ (Integral a, FiniteBits a) ⇒ A.MArray s → Int → a → ST s Int
-unsafeAppendDec marr off n = unsafePrependDec marr (off + exactDecLen n) n
-{-# INLINEABLE unsafeAppendDec #-}
+unsafeAppendBoundedDec ∷ (Integral a, FiniteBits a) ⇒ A.MArray s → Int → a → ST s Int
+unsafeAppendBoundedDec marr off n = unsafePrependBoundedDec marr (off + exactDecLen n) n
+{-# INLINEABLE unsafeAppendBoundedDec #-}
 
-unsafePrependDec ∷ ∀ s a. (Integral a, FiniteBits a) ⇒ A.MArray s → Int → a → ST s Int
-unsafePrependDec marr !off n
+unsafePrependBoundedDec ∷ ∀ s a. (Integral a, FiniteBits a) ⇒ A.MArray s → Int → a → ST s Int
+unsafePrependBoundedDec marr !off n
   | n < 0
   , n == bit (finiteBitSize n - 1) = do
       A.unsafeWrite marr (off - 1) (fromIntegral (0x30 + minBoundLastDigit n))
@@ -98,15 +110,15 @@ unsafePrependDec marr !off n
     go o k
       | k >= 10 = do
           let (q, r) = quotRem100 k
-          A.copyFromPointer marr (o - 1) (Ptr digits `plusPtr` (fromIntegral r `shiftL` 1)) 2
+          A.copyFromPointer marr (o - 1) (digits `plusPtr` (fromIntegral r `shiftL` 1)) 2
           if k < 100 then pure (o - 1) else go (o - 2) q
       | otherwise = do
           A.unsafeWrite marr o (fromIntegral (0x30 + k))
           pure o
+{-# INLINEABLE unsafePrependBoundedDec #-}
 
-    digits ∷ Addr#
-    digits = "00010203040506070809101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960616263646566676869707172737475767778798081828384858687888990919293949596979899"#
-{-# INLINEABLE unsafePrependDec #-}
+digits ∷ CString
+digits = Ptr "00010203040506070809101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960616263646566676869707172737475767778798081828384858687888990919293949596979899"#
 
 -- Compute rem minBound 10 efficiently. Given that:
 -- • minBound = 1 `shiftL` (finiteBitSize a - 1) = -2^(finiteBitSize a - 1)
@@ -170,3 +182,99 @@ quotBillion a = case (finiteBitSize a, isSigned a) of
     cast f = fromIntegral (f (fromIntegral a))
 #endif
 {-# INLINEABLE quotBillion #-}
+
+--------------------------------------------------------------------------------
+-- Unbounded
+--------------------------------------------------------------------------------
+
+-- | Append the decimal representation of an /unbounded/ integral number.
+(|>$$) ∷ Integral a ⇒ Buffer ⊸ a → Buffer
+
+infixl 6 |>$$
+buffer |>$$ n = case toInteger n of
+  n' →
+    appendBounded
+      (maxIntegerDecLen n')
+      (\dst dstOff → unsafeAppendUnboundedDec dst dstOff n')
+      buffer
+{-# INLINEABLE (|>$$) #-}
+
+-- | Prepend the decimal representation of an /unbounded/ integral number.
+($$<|) ∷ Integral a ⇒ a → Buffer ⊸ Buffer
+
+infixr 6 $$<|
+n $$<| buffer = case toInteger n of
+  n' →
+    prependBounded
+      (maxIntegerDecLen n')
+      (\dst dstOff → unsafePrependUnboundedDec dst dstOff n')
+      (\dst dstOff → unsafeAppendUnboundedDec dst dstOff n')
+      buffer
+{-# INLINEABLE ($$<|) #-}
+
+-- | ceiling (fbs a * logBase 10 2) < ceiling (fbs a * 5 / 16) < 1 + floor (fbs a * 5 / 16)
+maxIntegerDecLen ∷ Integer → Int
+maxIntegerDecLen a = case a of
+  I.IS i# → maxDecLen (I# i#)
+  I.IP n# → 1 + (fromIntegral (BN.bigNatSize n#) * finiteBitSize (0 ∷ Word) * 5) `shiftR` 4
+  I.IN n# → 2 + (fromIntegral (BN.bigNatSize n#) * finiteBitSize (0 ∷ Word) * 5) `shiftR` 4
+{-# INLINEABLE maxIntegerDecLen #-}
+
+exactIntegerDecLen ∷ Integer → Int
+exactIntegerDecLen n = case n of
+  I.IS i# → exactDecLen (I# i#)
+  I.IP n# → fromIntegral (BN.bigNatSizeInBase 10 n#)
+  I.IN n# → 1 + fromIntegral (BN.bigNatSizeInBase 10 n#)
+
+unsafeAppendUnboundedDec ∷ A.MArray s → Int → Integer → ST s Int
+unsafeAppendUnboundedDec marr off n = unsafePrependUnboundedDec marr (off + exactIntegerDecLen n) n
+{-# INLINEABLE unsafeAppendUnboundedDec #-}
+
+unsafePrependUnboundedDec ∷ ∀ s. A.MArray s → Int → Integer → ST s Int
+unsafePrependUnboundedDec marr !off n = case toInteger n of
+  I.IS i# → unsafePrependBoundedDec marr off (I# i#)
+  I.IP n# → go (off - 1) n# >>= \o → pure (off - o)
+  I.IN n# → go (off - 1) n# >>= sign
+  where
+    sign !o = do
+      A.unsafeWrite marr (o - 1) 0x2d -- '-'
+      pure (off - o + 1)
+
+    -- TODO: remove once best implementation found
+    -- go ∷ Int → BN.BigNat# → ST s Int
+    -- go o n# = case BN.bigNatCompareWord# n# 10## of
+    --   LT → do
+    --     A.unsafeWrite marr o (fromIntegral (0x30 + BN.bigNatToWord n#))
+    --     pure o
+    --   _ → do
+    --     let !(# q#, r## #) = BN.bigNatQuotRemWord# n# 100##
+    --     A.copyFromPointer marr (o - 1) (digits `plusPtr` (fromIntegral (W# r##) `shiftL` 1)) 2
+    --     case BN.bigNatCompareWord# n# 100## of
+    --       LT → pure (o - 1)
+    --       _ → go (o - 2) q#
+
+    go ∷ Int → BN.BigNat# → ST s Int
+    go o n# = do
+      let !(# q#, r## #) = BN.bigNatQuotRemWord# n# 100##
+      A.copyFromPointer marr (o - 1) (digits `plusPtr` (fromIntegral (W# r##) `shiftL` 1)) 2
+      -- TODO: remove once best implementation found
+      -- case BN.bigNatToWordMaybe# q# of
+      --   (# | w# #) → goWord (o - 2) (W# w#)
+      --   _ → go (o - 2) q#
+      if BN.bigNatIsZero q#
+        then pure (o - 2)
+        else
+          if isTrue# (BN.bigNatSize# q# ># 1#)
+            then go (o - 2) q#
+            else goWord (o - 2) (W# (BN.bigNatIndex# q# 0#))
+
+    goWord ∷ Int → Word → ST s Int
+    goWord o k
+      | k >= 10 = do
+          let (q, r) = quotRem100 k
+          A.copyFromPointer marr (o - 1) (digits `plusPtr` (fromIntegral r `shiftL` 1)) 2
+          if k < 100 then pure (o - 1) else goWord (o - 2) q
+      | otherwise = do
+          A.unsafeWrite marr o (fromIntegral (0x30 + k))
+          pure o
+{-# INLINEABLE unsafePrependUnboundedDec #-}
