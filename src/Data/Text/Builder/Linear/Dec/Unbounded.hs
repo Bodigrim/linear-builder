ac822db6669afb33a37ccce38b7312b711c42245
Bodigrim <andrew.lelechenko@gmail.com>
2024-07-10 19:51:36 +0100

    Revert "Submit linear-builder-find-crossover"
    This reverts commit fcad0c8d7646b34dc28adb0a4196b7f740e47b83.

--- src/Data/Text/Builder/Linear/Dec/Unbounded.hs
+++ src/Data/Text/Builder/Linear/Dec/Unbounded.hs
@@ -5,8 +5,8 @@
 module Data.Text.Builder.Linear.Dec.Unbounded (
   (|>$$),
   ($$<|),
-  prependUnboundedDecimal,
-  Strategy (..),
+  -- prependUnboundedDecimal,
+  -- Strategy (..),
 )
 where
 
@@ -285,30 +285,30 @@ padWithZeros marr off count = unsafeReplicate marr off count 0x30
 -- For testing purpose only
 --------------------------------------------------------------------------------
 
-data Strategy = SmallOnly | HugeOnly
+-- data Strategy = SmallOnly | HugeOnly
 
-prependUnboundedDecimal ∷ Integral a ⇒ Strategy → a → Buffer ⊸ Buffer
-prependUnboundedDecimal strategy n buffer = case toInteger n of
-  !n' →
-    prependBounded'
-      (maxIntegerDecLen n')
-      (\dst dstOff → unsafePrependDec' strategy dst dstOff n')
-      buffer
+-- prependUnboundedDecimal ∷ Integral a ⇒ Strategy → a → Buffer ⊸ Buffer
+-- prependUnboundedDecimal strategy n buffer = case toInteger n of
+--   !n' →
+--     prependBounded'
+--       (maxIntegerDecLen n')
+--       (\dst dstOff → unsafePrependDec' strategy dst dstOff n')
+--       buffer
 
-unsafePrependDec' ∷ ∀ s. Strategy → A.MArray s → Int → Integer → ST s Int
-unsafePrependDec' s marr off@(I# off#) n' = case n' of
-  I.IS i# → Bounded.unsafePrependDec marr off (I# i#)
-  _ → unsafePrependBigNatDec' s marr (off# -# 1#) (integerToBigNat# n') >>= prependSign
-    where
-      prependSign !off' =
-        if n' < 0
-          then do
-            A.unsafeWrite marr (off' - 1) 0x2d -- '-'
-            pure (off - off' + 1)
-          else pure (off - off')
-{-# INLINEABLE unsafePrependDec' #-}
+-- unsafePrependDec' ∷ ∀ s. Strategy → A.MArray s → Int → Integer → ST s Int
+-- unsafePrependDec' s marr off@(I# off#) n' = case n' of
+--   I.IS i# → Bounded.unsafePrependDec marr off (I# i#)
+--   _ → unsafePrependBigNatDec' s marr (off# -# 1#) (integerToBigNat# n') >>= prependSign
+--     where
+--       prependSign !off' =
+--         if n' < 0
+--           then do
+--             A.unsafeWrite marr (off' - 1) 0x2d -- '-'
+--             pure (off - off' + 1)
+--           else pure (off - off')
+-- {-# INLINEABLE unsafePrependDec' #-}
 
-unsafePrependBigNatDec' ∷ ∀ s. Strategy → A.MArray s → DigitsWriter s
-unsafePrependBigNatDec' strategy marr !off0 !n0 = case strategy of
-  SmallOnly → prependSmallNat marr off0 n0
-  HugeOnly → prependHugeNat marr off0 n0
+-- unsafePrependBigNatDec' ∷ ∀ s. Strategy → A.MArray s → DigitsWriter s
+-- unsafePrependBigNatDec' strategy marr !off0 !n0 = case strategy of
+--   SmallOnly → prependSmallNat marr off0 n0
+--   HugeOnly → prependHugeNat marr off0 n0
