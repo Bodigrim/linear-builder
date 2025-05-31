413a95372d50e3843ce507d1cb2839103a399709
Bodigrim <andrew.lelechenko@gmail.com>
2025-05-31 18:10:53 +0300

    Revert "Hexadecimal: Add benchmarks for signed and unsigned numbers"
    This reverts commit b98e02cc28cbb92ea91c1e5408508fd1fb2cc16a.
    
    # Conflicts:
    #	bench/BenchHexadecimal.hs

--- bench/BenchHexadecimal.hs
+++ bench/BenchHexadecimal.hs
@@ -21,50 +21,42 @@ import qualified TextBuilder
 word :: Word
 word = 123456789123456789
 
-benchLazyBuilder ∷ Word → T.Text
+benchLazyBuilder ∷ Int → T.Text
 benchLazyBuilder = toStrict . toLazyText . go mempty
   where
     go !acc 0 = acc
-    go !acc n = let i = n * word in go (hexadecimal i <> (acc <> hexadecimal i)) (n - 1)
+    go !acc n = let i = fromIntegral n * word in go (hexadecimal i <> (acc <> hexadecimal i)) (n - 1)
 
-benchLazyBuilderBS ∷ Word → B.ByteString
+benchLazyBuilderBS ∷ Int → B.ByteString
 benchLazyBuilderBS = B.toStrict . B.toLazyByteString . go mempty
   where
     go !acc 0 = acc
-    go !acc n = go (B.wordHex n <> (acc <> B.wordHex n)) (n - 1)
+    go !acc n = go (B.wordHex (fromIntegral n) <> (acc <> B.wordHex (fromIntegral n))) (n - 1)
 
 #ifdef MIN_VERSION_text_builder
-benchStrictBuilder ∷ Word → T.Text
+benchStrictBuilder ∷ Int → T.Text
 benchStrictBuilder = TextBuilder.toText . go mempty
   where
     go !acc 0 = acc
-    go !acc n = let i = n * word in go (TextBuilder.hexadecimal i <> (acc <> TextBuilder.hexadecimal i)) (n - 1)
+    go !acc n = let i = fromIntegral n * word in go (TextBuilder.hexadecimal i <> (acc <> TextBuilder.hexadecimal i)) (n - 1)
 #endif
 
-benchLinearBuilderWord ∷ Word → T.Text
-benchLinearBuilderWord m = runBuffer (\b → go b m)
-  where
-    go ∷ Buffer ⊸ Word → Buffer
-    go !acc 0 = acc
-    go !acc n = let i = n * word in go (i &<| (acc |>& i)) (n - 1)
-
-benchLinearBuilderInt ∷ Word → T.Text
-benchLinearBuilderInt m = runBuffer (\b → go b (fromIntegral m))
+benchLinearBuilder ∷ Int → T.Text
+benchLinearBuilder m = runBuffer (\b → go b m)
   where
     go ∷ Buffer ⊸ Int → Buffer
     go !acc 0 = acc
-    go !acc n = let i = n * fromIntegral word in go (i &<| (acc |>& i)) (n - 1)
+    go !acc n = let i = fromIntegral n * word in go (i &<| (acc |>& i)) (n - 1)
 
 benchHexadecimal ∷ Benchmark
 benchHexadecimal = bgroup "Hexadecimal" $ map mkGroup [1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6]
 
-mkGroup :: Word → Benchmark
+mkGroup :: Int → Benchmark
 mkGroup n = bgroup (show n)
   [ bench "Data.Text.Lazy.Builder" $ nf benchLazyBuilder n
   , bench "Data.ByteString.Builder" $ nf benchLazyBuilderBS n
 #ifdef MIN_VERSION_text_builder
   , bench "TextBuilder" $ nf benchStrictBuilder n
 #endif
-  , bench "Data.Text.Builder.Linear (Word)" $ nf benchLinearBuilderWord n
-  , bench "Data.Text.Builder.Linear (Int)" $ nf benchLinearBuilderInt n
+  , bench "Data.Text.Builder.Linear" $ nf benchLinearBuilder n
   ]
